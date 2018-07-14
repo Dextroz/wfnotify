22fcfcf7f7c03b7f25798016dc5e8b41313a9de9
Dextroz <dextrozmlg@gmail.com>
2018-07-14 20:41:42 +0100

    Updated Readme, added basic logo, added basic funtionality for each button.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # wfnotify
-A PyQt5 app for fetching and viewing Warframe API data.
+![wfnotify](my_logo.png)
+A PyQt5 app for viewing Warframe API data.
+
+## Disclaimer
+* This build of the app is currently under active development and is not final.
+* I did not create PyQt5. See [Acknowledgments](#Acknowledgments)
+
+## Current Features
+  1. Fetch current Cetustime (Check if night or day).
+  2. Fetch current alerts data.
+  3. Fetch current fissure data.
+  4. Fetch current sortie data.
+
+## Dependencies
+wfnotify is written in Python 3 so it is **REQUIRED**.
+
+wfnotify requires the following dependencies:
+  1. * [PyQt5](https://riverbankcomputing.com/software/pyqt/download5) - A Python application framework.
+
+## Authors -- Contributors
+
+* **Daniel Brennand** - *Author of project* - [Dextroz](https://github.com/Dextroz)
+
+## License
+
+This project is licensed under the GPL-3.0 License - see the [LICENSE](LICENSE) for details.
+
+## Acknowledgments
+Riverbank Computing Limited created [PyQt5](https://riverbankcomputing.com/software/pyqt/download5)
--- wfnotify.py
+++ wfnotify.py
@@ -1,13 +1,12 @@
 try:
     # Imports required for GUI application.
     import sys
-    from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QVBoxLayout
+    from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QVBoxLayout, QTextEdit
     from PyQt5.QtCore import pyqtSlot
+    from PyQt5.QtGui import QIcon
     # For contacting Warframe API.
     import requests
-    # Partial used to pass additional args into function for button.
-    # See: https://docs.python.org/3/library/functools.html#functools.partial
-    from functools import partial
+    # Partial removed. No longer used.
 except ImportError:
     print("Failed to import required packages.")
 
@@ -22,12 +21,19 @@ class App(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle("Warframe Notifier")
-        self.setGeometry(10, 10, 320, 100)
+        self.setWindowIcon(QIcon("my_logo.png"))
+        self.setWindowTitle("wfnotify")
+        self.setGeometry(10, 10, 500, 200)
 
         # Adding groupbox (returned from h_layout() to window.)
         window_layout = QVBoxLayout()
         window_layout.addWidget(self.h_layout())
+
+        # Adding Text box.
+        self.text_box = QTextEdit()
+        window_layout.addWidget(self.text_box)
+
+        # Set Layout.
         self.setLayout(window_layout)
 
         # Show the window upon execution.
@@ -41,23 +47,23 @@ class App(QWidget):
 
         # Defining widgets are appending them to the Horisontal box layout.
         button_worldstate = QPushButton("Check Cetuscycle", self)
-        button_worldstate.clicked.connect(
-            partial(self.get_request, endpoint=self.endpoint_list[0]))
+        button_worldstate.clicked.connect(lambda: self.cetus_text(
+            self.get_request(self.endpoint_list[0])))
         h_box.addWidget(button_worldstate)
 
         button_alerts = QPushButton("Check Alerts", self)
-        button_alerts.clicked.connect(
-            partial(self.get_request, endpoint=self.endpoint_list[1]))
+        button_alerts.clicked.connect(lambda: self.alert_text(
+            self.get_request(self.endpoint_list[1])))
         h_box.addWidget(button_alerts)
 
         button_fissures = QPushButton("Check Fissures", self)
-        button_fissures.clicked.connect(
-            partial(self.get_request, endpoint=self.endpoint_list[2]))
+        button_fissures.clicked.connect(lambda: self.fissures_text(
+            self.get_request(self.endpoint_list[2])))
         h_box.addWidget(button_fissures)
 
         button_sortie = QPushButton("Check Sortie", self)
-        button_sortie.clicked.connect(
-            partial(self.get_request, endpoint=self.endpoint_list[3]))
+        button_sortie.clicked.connect(lambda: self.sortie_text(
+            self.get_request(self.endpoint_list[3])))
         h_box.addWidget(button_sortie)
 
         # Adding the horisontal box layout to the groupbox object and returning
@@ -71,15 +77,98 @@ class App(QWidget):
         try:
             request = requests.get(endpoint)
             if ((request.status_code) == (requests.codes.ok)):
-                print(request.json())
+                return request.json()
             else:
                 request.raise_for_status()
         except requests.exceptions.TooManyRedirects:
-            print("Request exceeded the acceptable number of redirects.")
+            self.text_box.setText(
+                "Request exceeded the acceptable number of redirects.")
         except requests.exceptions.Timeout:
-            print("Request timed out.")
+            self.text_box.setText("Request timed out.")
         except requests.exceptions.HTTPError as err:
-            print(f"The following HTTPError occured {err}")
+            self.text_box.setText(f"The following HTTPError occured {err}")
+
+    @pyqtSlot()
+    def cetus_text(self, json_resp):
+        try:
+            short_string = json_resp["shortString"]
+            isday = json_resp["isDay"]
+            if ((isday) == (True)):
+                self.text_box.setText(
+                    f"It's currently day with {short_string}")
+            else:
+                self.text_box.setText(
+                    f"It's currently night with {short_string}")
+        except KeyError as err:
+            self.text_box.setText(f"The following KeyError occured {err}")
+
+    @pyqtSlot()
+    def alert_text(self, json_resp):
+        try:
+            # Clear Text box before start of func.
+            self.text_box.setText("")
+            # Collect relevant data and store in respective variables.
+            for num, alerts in enumerate(json_resp, 1):
+                mission_location = alerts["mission"]["node"]
+                mission_type = alerts["mission"]["type"]
+                eta = alerts["eta"]
+                max_enemy_lvl = alerts["mission"]["maxEnemyLevel"]
+                faction = alerts["mission"]["faction"]
+                reward = alerts["mission"]["reward"]["asString"]
+                format_str = f"""{num}.
+    Location: {mission_location}
+    Type: {mission_type}
+    ETA: {eta}
+    Enemy Lvl: {max_enemy_lvl}
+    Faction: {faction}
+    Reward: {reward} \n"""
+                self.text_box.append(format_str)
+        except KeyError as err:
+            self.text_box.setText(f"The following KeyError occured {err}")
+
+    @pyqtSlot()
+    def fissures_text(self, json_resp):
+        try:
+            # Clear Text box before start of func.
+            self.text_box.setText("")
+            # Collect relevant data and store in respective variables.
+            for num, fissures in enumerate(json_resp, 1):
+                mission_location = fissures["node"]
+                mission_type = fissures["missionType"]
+                eta = fissures["eta"]
+                tier = fissures["tier"]
+                faction = fissures["enemy"]
+                format_str = f"""{num}.
+    Location: {mission_location}
+    Type: {mission_type}
+    ETA: {eta}
+    Tier: {tier}
+    Faction: {faction} \n"""
+                self.text_box.append(format_str)
+        except KeyError as err:
+            self.text_box.setText(f"The following KeyError occured {err}")
+
+    @pyqtSlot()
+    def sortie_text(self, json_resp):
+        try:
+            # Clear Text box before start of func.
+            self.text_box.setText("")
+            # Collect relevant data and store in respective variables.
+            eta = json_resp["eta"]
+            faction = json_resp["faction"]
+            for num, missions in enumerate(json_resp["variants"], 1):
+                mission_location = missions["node"]
+                mission_type = missions["missionType"]
+                modifier = missions["modifier"]
+                format_str = f"""{num}.
+    Location: {mission_location}
+    Type: {mission_type}
+    ETA: {eta}
+    Modifier: {modifier}
+    Faction: {faction} \n"""
+                self.text_box.append(format_str)
+        except KeyError as err:
+            self.text_box.setText(f"The following KeyError occured {err}")
 
 
 if __name__ == "__main__":
